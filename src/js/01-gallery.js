014fc6dc6fc23570159a4fe6e1a3f40dd89e1151
figatix <60873339+figatix@users.noreply.github.com>
2022-12-26 23:40:56 +0200

    /

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imagesMarkup = galleryItems.map(({ preview, original, description }) => {
+  return `<a class="gallery__item" href = "${original}">
+      <img
+        class="gallery__image"
+        src="${preview}" 
+        alt="${description}"
+      />
+    </a>`
+})
+const imagesMarkupString = imagesMarkup.join('')
+
+const galleryEl = document.querySelector('.gallery')
+
+galleryEl.insertAdjacentHTML('beforeend', imagesMarkupString)
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: "alt",
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  overlayOpacity: 0.8,
+
+});
+
