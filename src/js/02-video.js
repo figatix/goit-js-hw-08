b0041f361dfd328e70d98e363fadc9d227770afc
figatix <60873339+figatix@users.noreply.github.com>
2022-12-29 23:14:02 +0200

    clean comments

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,35 +1,19 @@
 import Vimeo from '@vimeo/player'
-// Player = require("@vimeo/player")
+const throttle = require('lodash.throttle');
 
 
 const iframe = document.querySelector('#vimeo-player');
-
-console.log('iframe', iframe);
-
 const player = new Vimeo(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
 
-player.on('timeupdate', onTimeupdate)
-
-
-
-player.getVideoTitle().then(function(title) {
-    console.log('title:', title);
-});
+const onTimeupdateThrottle = throttle(onTimeupdate, 1000)
 
 function onTimeupdate(e) {
-  console.log(e);
   const stringifyTimeupdate = JSON.stringify(e)
-
   localStorage.setItem('videoplayer-current-time', stringifyTimeupdate)
 }
 
-// const a = { seconds: 14.125, percent: 0.025, duration: 571.563 };
-// const bParse = JSON.stringify(a);
-// console.log(bParse);
+player.on('timeupdate', onTimeupdateThrottle)
 
 const currentTimeLastView = localStorage.getItem('videoplayer-current-time');
 
@@ -37,19 +21,14 @@ try {
   const parseCurrentTimeLastView = JSON.parse(currentTimeLastView);
   const currentSecondLastView = parseCurrentTimeLastView.seconds
 
-  player.setCurrentTime(currentSecondLastView).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
+  player.setCurrentTime(currentSecondLastView).then(function(seconds) {}).catch(function(error) {
     switch (error.name) {
       case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
           break;
-
       default:
-          // some other error occurred
           break;
     }
-});
+  });
 } catch (err) {
   console.log(err);
 }
