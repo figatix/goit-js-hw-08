16973bce25a7a00641f64551af2b03c23cb3924b
figatix <60873339+figatix@users.noreply.github.com>
2022-12-30 22:13:53 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
 
+const throttle = require('lodash.throttle');
+
+const formEl = document.querySelector('.feedback-form')
+const resultObj = {}
+
+function fillFormFields() {
+  const formInfoLS =  JSON.parse(localStorage.getItem('feedback-form-state'))
+
+  if (!formInfoLS) {
+    return
+  }
+
+  for (let prop in formInfoLS) {
+    formEl.elements[prop].value = formInfoLS[prop];
+  }
+}
+
+fillFormFields()
+
+
+function onInputText(e) {
+  resultObj[e.target.name] = e.target.value
+  const objStr = JSON.stringify(resultObj)
+  localStorage.setItem('feedback-form-state', objStr)
+}
+
+function onSubmit(e) {
+  e.preventDefault()
+  console.dir(JSON.parse(localStorage.getItem('feedback-form-state')))
+  localStorage.removeItem('feedback-form-state')
+  formEl.reset()
+}
+
+const onInputTextThrottle = throttle(onInputText, 500);
+const onSubmitThrottle = throttle(onSubmit, 500);
+
+formEl.addEventListener('input', onInputTextThrottle)
+formEl.addEventListener('submit', onSubmitThrottle)
